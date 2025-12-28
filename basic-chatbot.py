7ffb0b1c22e8c69929b08d200e151ae9d8bda7ae
gollasamatha <gollasamatha44@gmail.com>
2025-12-28 13:28:48 +0530

    Implement basic rule-based chatbot

---
+++ basic-chatbot.py
@@ -0,0 +1,22 @@
+# Basic Rule-Based Chatbot
+
+def chatbot():
+    print("Chatbot: Hi! Type 'bye' to exit.")
+
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Chatbot: Hi!")
+
+        elif user_input == "how are you":
+            print("Chatbot: I'm fine, thanks!")
+
+        elif user_input == "bye":
+            print("Chatbot: Goodbye!")
+            break
+
+        else:
+            print("Chatbot: Sorry, I didn't understand that.")
+
+chatbot()
